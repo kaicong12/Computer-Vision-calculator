d91c1a22f92f80bfc585a74f0c3c779527c80260
kctey <kaicong12@gmail.com>
2022-02-21 11:33:32 +0800

    added rebase sentence

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -2,6 +2,7 @@ import numpy as np
 from .metrics import InferenceCalculator
 
 
+# this is a rebased test
 def confusion_matrix_per_image(label_id_map, confusion_matrix, ground_truth_arr, preds_arr, iou_threshold):
     """
     When returning labels in viewset make sure the labels are returned in ascending id
